61fb494bec55bb9dd6b6cf3aa4532d5930f56a51
r0798566 <r0798566@G-D103-12.local.thomasmore.be>
2019-12-14 09:16:29 +0100

    DAO added for Artist and UserArtist

---
+++ Artmin/src/main/java/artmin/dao/ArtistDao.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package artmin.dao;
+
+import artmin.model.Artist;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.Query;
+
+/**
+ *
+ * @author r0798566
+ */
+public class ArtistDao extends AbstractDao<Long, Artist> {
+    public Artist findByid(long id){
+        return getByKey(id);
+    }
+    
+    public void saveUser(Artist artist) {
+        persist(artist);
+    }
+
+    public void deleteUserById(Long id) {
+        Query query = getSession().createSQLQuery("delete from artist where id = :id");
+        query.setLong("id", id);
+        query.executeUpdate();
+    }
+    
+    @SuppressWarnings("unchecked")
+    public List<Artist> findAllArtists() {
+        Criteria criteria = createEntityCriteria();
+        return (List<Artist>) criteria.list();
+    }
+}
